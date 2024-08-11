0d0fb35d487afdb038a8deadd1b7399fb44b4b85
iamstupid-crypo <79710224+iamstupid-crypo@users.noreply.github.com>
2024-08-11 09:58:53 +0530

    Addition of code files and necessary attachments

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('cropForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevents the form from submitting in the traditional way
+    alert('The best crop according to the data would be rice crop.');
+});
